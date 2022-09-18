84c6e57afa224b4097e7bb449f4af805afaf4cbb
Alexander Epifanov <alexlaw00@mail.ru>
2022-09-18 17:05:13 +0300

    Added SegmentRepository

---
+++ TicketSelling/TicketSelling.Core/Domains/Segments/Repositories/ISegmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using TicketSelling.Core.Domains.Tickets;
+
+namespace TicketSelling.Core.Domains.Segments.Repositories
+{
+    public interface ISegmentRepository
+    {
+        Task SaleTicket(SaleTicket ticket, CancellationToken token);
+        Task RefundSegmentsByTicketNumber(string ticketNumber, CancellationToken token);
+    }
+}
---
+++ TicketSelling/TicketSelling.Data/DbModels/Segments/Repositories/SegmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using TicketSelling.Core.Domains.Segments.Repositories;
+using TicketSelling.Core.Domains.Tickets;
+
+namespace TicketSelling.Data.DbModels.Segments.Repositories
+{
+    public class SegmentRepository : ISegmentRepository
+    {
+        public Task RefundSegmentsByTicketNumber(string ticketNumber, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaleTicket(SaleTicket ticket, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
