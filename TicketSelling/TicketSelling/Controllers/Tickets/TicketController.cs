d3e033db1d0e3a0ac92ba2115e89bc5b39bd51f7
Alexander Epifanov <alexlaw00@mail.ru>
2022-09-18 11:22:06 +0300

    Added calls to TicketService methods in TicketController

--- TicketSelling/TicketSelling/Controllers/Tickets/TicketController.cs
+++ TicketSelling/TicketSelling/Controllers/Tickets/TicketController.cs
@@ -1,5 +1,8 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using TicketSelling.Controllers.Tickets.Dto;
+using TicketSelling.Core.Domains.Tickets;
+using TicketSelling.Core.Domains.Tickets.Services;
 
 namespace TicketSelling.Controllers.Tickets
 {
@@ -8,16 +11,27 @@ namespace TicketSelling.Controllers.Tickets
     [ApiVersion("1.0")]
     public class TicketController : ControllerBase
     {
-        [HttpPost("sale/")]
-        public void Sale(SaleTicketDto model)
+        private readonly IMapper _mapper;
+        private readonly ITicketService _ticketService;
+
+        public TicketController(IMapper mapper, ITicketService ticketService)
         {
-            
+            _mapper = mapper;
+            _ticketService = ticketService;
+        }
+
+        [HttpPost("sale/")]
+        public async Task Sale(SaleTicketDto model, CancellationToken cancellationToken)
+        {
+            var saleTicket = _mapper.Map<SaleTicket>(model);
+            await _ticketService.SaleTicket(saleTicket, cancellationToken);
         }
 
         [HttpPost("refund/")]
-        public void Refund(RefundTicketDto model)
+        public async Task Refund(RefundTicketDto model, CancellationToken cancellationToken)
         {
-            
+            var refundTicket = _mapper.Map<RefundTicket>(model);
+            await _ticketService.RefundTicket(refundTicket, cancellationToken);
         }
     }
 }
