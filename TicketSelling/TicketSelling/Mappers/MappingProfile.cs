840d3e8bdc22fa73307e197c697c080f5d86410b
Alexander Epifanov <alexlaw00@mail.ru>
2022-09-18 11:16:08 +0300

    Added AutoMapper and MappingProfile

---
+++ TicketSelling/TicketSelling/Mappers/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using TicketSelling.Controllers.Tickets.Dto;
+using TicketSelling.Core.Domains.Passengers;
+using TicketSelling.Core.Domains.Segments;
+using TicketSelling.Core.Domains.Tickets;
+
+namespace TicketSelling.Mappers
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<PassengerDto, Passenger>();
+            CreateMap<RefundTicketDto, RefundTicket>();
+            CreateMap<SaleTicketDto, SaleTicket>();
+            CreateMap<SegmentDto, Segment>();
+        }
+    }
+}
