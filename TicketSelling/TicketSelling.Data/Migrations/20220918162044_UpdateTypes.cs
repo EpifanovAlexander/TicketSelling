6c569b2f258b75537f379d5c5fd5c54579dab692
Alexander Epifanov <alexlaw00@mail.ru>
2022-09-18 21:18:40 +0300

    Added UpdateTypes migration

---
+++ TicketSelling/TicketSelling.Data/Migrations/20220918162044_UpdateTypes.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketSelling.Data.Migrations
+{
+    public partial class UpdateTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ticket_number",
+                table: "segments",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ticket_number",
+                table: "segments",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
