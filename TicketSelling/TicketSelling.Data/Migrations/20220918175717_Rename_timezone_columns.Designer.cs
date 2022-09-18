f3a2a79386c7069f12949719df19a3745c0745d9
Alexander Epifanov <alexlaw00@mail.ru>
2022-09-18 21:19:20 +0300

    Added Rename_timezones migration

--- TicketSelling/TicketSelling.Data/Migrations/20220918175717_Rename_timezone_columns.Designer.cs
+++ TicketSelling/TicketSelling.Data/Migrations/20220918175717_Rename_timezone_columns.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TicketSelling.Data;
@@ -11,9 +12,10 @@ using TicketSelling.Data;
 namespace TicketSelling.Data.Migrations
 {
     [DbContext(typeof(TicketSellingContext))]
-    partial class TicketSellingContextModelSnapshot : ModelSnapshot
+    [Migration("20220918175717_Rename_timezone_columns")]
+    partial class Rename_timezone_columns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TicketSelling/TicketSelling.Data/Migrations/20220918175717_Rename_timezone_columns.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketSelling.Data.Migrations
+{
+    public partial class Rename_timezone_columns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DepartTimeZone",
+                table: "segments",
+                newName: "depart_datetime_timezone");
+
+            migrationBuilder.RenameColumn(
+                name: "ArriveTimeZone",
+                table: "segments",
+                newName: "arrive_datetime_timezone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "depart_datetime_timezone",
+                table: "segments",
+                newName: "DepartTimeZone");
+
+            migrationBuilder.RenameColumn(
+                name: "arrive_datetime_timezone",
+                table: "segments",
+                newName: "ArriveTimeZone");
+        }
+    }
+}
