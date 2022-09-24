3df6e01023704e38c2d26bad49fc726ec4db5f56
Alexander Epifanov <alexlaw00@mail.ru>
2022-09-24 23:52:40 +0300

    Added validation attributes for PassengerDto

--- TicketSelling/TicketSelling.Core/Domains/Passengers/Dto/PassengerDto.cs
+++ TicketSelling/TicketSelling.Core/Domains/Passengers/Dto/PassengerDto.cs
@@ -1,24 +1,32 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using TicketSelling.Core.ValidationAttributes;
 
 namespace TicketSelling.Core.Domains.Passengers.Dto
 {
+    [PassengerValidation]
     public class PassengerDto
     {
+        [Required(ErrorMessage = "Имя пассажира не может быть пустым")]
         [JsonPropertyName("name")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Фамилия пассажира не может быть пустой")]
         [JsonPropertyName("surname")]
         public string Surname { get; set; }
 
         [JsonPropertyName("patronymic")]
         public string Patronymic { get; set; }
 
+        [Required(ErrorMessage = "Тип документа должен быть определён")]
         [JsonPropertyName("doc_type")]
         public string DocumentType { get; set; }
 
+        [Required(ErrorMessage = "Номер документа должен быть определён")]
         [JsonPropertyName("doc_number")]
         public string DocumentNumber { get; set; }
 
+        [Required(ErrorMessage = "Дата рождения пассажира не может быть пустой")]
         [JsonPropertyName("birthdate")]
         public DateTime Birthdate { get; set; }
 
@@ -28,9 +36,12 @@ namespace TicketSelling.Core.Domains.Passengers.Dto
         [JsonPropertyName("passenger_type")]
         public string PassengerType { get; set; }
 
+        [Required(ErrorMessage = "Номер билета должен быть определён")]
+        [RegularExpression(@"^(\d{13})$", ErrorMessage = "Невалидный номер билета")]
         [JsonPropertyName("ticket_number")]
         public string TicketNumber { get; set; }
 
+        [Required(ErrorMessage = "Тип билета должен быть определён")]
         [JsonPropertyName("ticket_type")]
         public int TicketType { get; set; }
 
