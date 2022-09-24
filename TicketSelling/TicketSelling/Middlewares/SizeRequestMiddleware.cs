7b85acb81b3895faf7b6bff8c5ed229afee79206
Alexander Epifanov <alexlaw00@mail.ru>
2022-09-24 16:47:08 +0300

    Added size request middleware

---
+++ TicketSelling/TicketSelling/Middlewares/SizeRequestMiddleware.cs
@@ -0,0 +1,27 @@
+﻿namespace TicketSelling.Middlewares
+{
+    public class SizeRequestMiddleware
+    {
+        private const long REQUEST_SIZE_LIMIT = 2 * 1024;
+
+        public readonly RequestDelegate next;
+
+        public SizeRequestMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Headers.ContentLength > REQUEST_SIZE_LIMIT)
+            {
+                httpContext.Response.StatusCode = StatusCodes.Status411LengthRequired;
+                await httpContext.Response.WriteAsJsonAsync(new { Message = "Недопустимая длина запроса" });
+            }
+            else 
+            {
+                await next(httpContext);
+            }
+        }
+    }
+}
--- TicketSelling/TicketSelling/Program.cs
+++ TicketSelling/TicketSelling/Program.cs
@@ -38,6 +38,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<SizeRequestMiddleware>();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
