8d214b28e91596c97f0910015470eaf05ffde6da
Alexander Epifanov <alexlaw00@mail.ru>
2022-09-27 00:42:23 +0300

    Realized request timeout methods

--- TicketSelling/TicketSelling/Controllers/Tickets/TicketController.cs
+++ TicketSelling/TicketSelling/Controllers/Tickets/TicketController.cs
@@ -8,7 +8,9 @@ namespace TicketSelling.Controllers.Tickets
     [ApiController]
     [ApiVersion("1.0")]
     public class TicketController : ControllerBase
-    {    
+    {
+        private const int REQUEST_TIMEOUT = 120 * 1000;
+
         private readonly ITicketService _ticketService;
 
         public TicketController(ITicketService ticketService)
@@ -16,16 +18,38 @@ namespace TicketSelling.Controllers.Tickets
             _ticketService = ticketService;
         }
 
+        private async Task Timeout()
+        {
+            await Task.Delay(REQUEST_TIMEOUT);
+        }
+
+        private async Task RunWithTimeout<T>(Func<T, CancellationToken, Task> ticketAsyncOperation, T ticketDto, CancellationToken cancellationToken) where T : class
+        {
+            var work = ticketAsyncOperation.Invoke(ticketDto, cancellationToken);
+            var timeout = Timeout();
+            var finishedTask = await Task.WhenAny(timeout, work);
+            if (finishedTask == timeout)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                throw new TimeoutException();
+            }
+            else 
+            {
+                if (finishedTask.Exception != null && finishedTask.Exception.InnerException != null)
+                    throw finishedTask.Exception.InnerException;
+            }
+        }
+
         [HttpPost("sale/")]
         public async Task Sale(SaleTicketDto saleTicketDto, CancellationToken cancellationToken)
         {
-            await _ticketService.SaleTicketAsync(saleTicketDto, cancellationToken);
+            await RunWithTimeout(_ticketService.SaleTicketAsync, saleTicketDto, cancellationToken);
         }
 
         [HttpPost("refund/")]
         public async Task Refund(RefundTicketDto refundTicketDto, CancellationToken cancellationToken)
         {
-            await _ticketService.RefundTicketAsync(refundTicketDto, cancellationToken);
+            await RunWithTimeout(_ticketService.RefundTicketAsync, refundTicketDto, cancellationToken);
         }
     }
 }
