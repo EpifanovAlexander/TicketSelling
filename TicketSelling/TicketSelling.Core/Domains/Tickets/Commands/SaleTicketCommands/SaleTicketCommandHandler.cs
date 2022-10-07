b7a1e22233a57a0d26711fc73cd6197a440f80ff
Alexander Epifanov <alexlaw00@mail.ru>
2022-10-07 20:30:31 +0300

    Added command and commandHandler for sale ticket operation

---
+++ TicketSelling/TicketSelling.Core/Domains/Tickets/Commands/SaleTicketCommands/SaleTicketCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using TicketSelling.Core.Domains.Tickets.Services;
+
+namespace TicketSelling.Core.Domains.Tickets.Commands.SaleTicketCommands
+{
+    public class SaleTicketCommandHandler : IRequestHandler<SaleTicketCommand, Unit>
+    {
+        private readonly ITicketService _ticketService;
+
+        public SaleTicketCommandHandler(ITicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+
+        public async Task<Unit> Handle(SaleTicketCommand request, CancellationToken cancellationToken)
+        {
+            await _ticketService.SaleTicketAsync(request, cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
