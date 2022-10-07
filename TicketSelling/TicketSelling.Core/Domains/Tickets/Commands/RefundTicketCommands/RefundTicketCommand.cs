da8d7503e98e27caf5951326994aaa900e0e1458
Alexander Epifanov <alexlaw00@mail.ru>
2022-10-07 20:30:05 +0300

    Added command and commandHandler for refund ticket operation

---
+++ TicketSelling/TicketSelling.Core/Domains/Tickets/Commands/RefundTicketCommands/RefundTicketCommand.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace TicketSelling.Core.Domains.Tickets.Commands.RefundTicketCommands
+{
+    public class RefundTicketCommand : IRequest<Unit>
+    {
+        [Required(ErrorMessage = "Тип операции должен быть определён")]
+        [JsonPropertyName("operation_type")]
+        public string OperationType { get; private set; }
+
+        [JsonPropertyName("operation_time")]
+        public DateTimeOffset OperationTime { get; private set; }
+
+        [JsonPropertyName("operation_place")]
+        public string OperationPlace { get; private set; }
+
+        [Required(ErrorMessage = "Номер билета должен быть определён")]
+        [RegularExpression(@"^(\d{13})$", ErrorMessage = "Невалидный номер билета")]
+        [JsonPropertyName("ticket_number")]
+        public string TicketNumber { get; private set; }
+
+        public RefundTicketCommand(string operationType, DateTimeOffset operationTime, string operationPlace, string ticketNumber)
+        {
+            OperationType = operationType;
+            OperationTime = operationTime;
+            OperationPlace = operationPlace;
+            TicketNumber = ticketNumber;
+        }
+    }
+}
---
+++ TicketSelling/TicketSelling.Core/Domains/Tickets/Commands/RefundTicketCommands/RefundTicketCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using TicketSelling.Core.Domains.Tickets.Services;
+
+namespace TicketSelling.Core.Domains.Tickets.Commands.RefundTicketCommands
+{
+    public class RefundTicketCommandHandler : IRequestHandler<RefundTicketCommand, Unit>
+    {
+        private readonly ITicketService _ticketService;
+
+        public RefundTicketCommandHandler(ITicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+
+        public async Task<Unit> Handle(RefundTicketCommand request, CancellationToken cancellationToken)
+        {
+            await _ticketService.RefundTicketAsync(request, cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
