e0d036f224a21ed56b8d0e2be87d926b65e3500a
Alexander Epifanov <alexlaw00@mail.ru>
2022-10-07 20:28:30 +0300

    Added MediatR DI to core project

--- TicketSelling/TicketSelling.Core/Bootstraps.cs
+++ TicketSelling/TicketSelling.Core/Bootstraps.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 using TicketSelling.Core.Domains.Tickets.Services;
 
 namespace TicketSelling.Core
@@ -7,6 +9,7 @@ namespace TicketSelling.Core
     {
         public static IServiceCollection AddCore(this IServiceCollection services)
         {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddScoped<ITicketService, TicketService>();
             return services;
         }
